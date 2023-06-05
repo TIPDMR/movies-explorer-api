8b9cb87d428bb5f8d72471c1b25096ea8cbf37d7
Dmitriy Tipsin <tipsin.dm@yandex.ru>
2023-06-05 13:15:47 +0300

    fix: Добавлены домены для CORS

--- utils/constants.js
+++ utils/constants.js
@@ -1,4 +1,18 @@
 module.exports.DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 module.exports.ALLOWED_DOMAIN_NAME = [
+  'http://localhost:3001',
+  'https://localhost:3001',
+
   'http://localhost:3000',
+  'https://localhost:3000',
+
+  'http://80.243.140.219:3000',
+  'https://80.243.140.219:3000',
+
+  'http://80.243.140.219:3001',
+  'https://80.243.140.219:3001',
+
+  'http://api.movies.best-mesto.ru',
+  'https://api.movies.best-mesto.ru',
+
 ];
